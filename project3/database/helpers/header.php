526fb1e63d1f52cc12db011e3a4ad7e0d7511720
Stephen Morrison <srmorrisonjit@gmail.com>
2014-03-14 15:10:28 -0400

    adding helper folder

---
+++ project3/database/helpers/header.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+
+  ini_set('display_errors', 'On');
+  error_reporting(E_ALL);
+?>
+
+<html>
+  <head>
+    <title><?php echo $pageTitle; ?></title>
+  </head>
+  <body>
+
