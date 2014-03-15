24ba78cb7945e9f48a4975792fdc53618888b093
Stephen Morrison <srmorrisonjit@gmail.com>
2014-03-14 20:28:08 -0400

    adding number of rows returned functionality and testing query forming

--- project3/loginsubmit.php
+++ project3/loginsubmit.php
@@ -4,30 +4,28 @@ include('helpers/header.php');
 $userEmail = $_POST['email'];
 $userPassword = $_POST['password'];
 
-$query = sprintf("SELECT Id, Name FROM Users WHERE Email='%s' AND Password=SHA1('%s') LIMIT 1", mysql_real_escape_string($userEmail), mysql_real_escape_string($userPassword));
+// $query = sprintf("SELECT Id, Name FROM Users WHERE Email='%s' AND Password=SHA1('%s') LIMIT 1", mysql_real_escape_string($userEmail), mysql_real_escape_string($userPassword));
 
-$db->makeQuery($query);
+$query = "SELECT Id, Name FROM Users WHERE Email=? AND Password=SHA1(?) LIMIT 1"
+
+$db->makeQuery($query, $userEmail, $userPassword);
 
 if (!$db->result) {
   die("Invalid Query: " . $db->result);
 }
 
-// $numRows = mysql_num_rows($db->result);
-//
-// if($numRows == 1) {
+if($db->numRowsReturned == 1) {
   foreach ($db->result as &$row) {
     $userId = $row['Id'];
     $userName = $row['Name'];
     $_SESSION["Id"] = $userId;
   }
-// }
-// else {
-//   die("User does not exist");
-// }
+}
+else {
+  die("User does not exist");
+}
 
 print "Welcome $userName";
-
-mysql_free_result($db->result);
 
 include("helpers/footer.php");
 ?>
