57a55c2bb624035aa29b3c624ce18e936253effc
Stephen Morrison <srmorrisonjit@gmail.com>
2014-03-17 15:39:19 -0400

    'removing account and starting addCourse'

--- account.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$hostname = "us-cdbr-east-05.cleardb.net" ;
-$username = "bc05f276159d04" ;
-$password = "cab4ffa7" ;
-$database  = "heroku_ad3d13b38fcc39e" ;
-
-?>
---
+++ project3/addCourse.php
@@ -0,0 +1,51 @@
+<?php
+  $pageTitle = "Add Course";
+  include("helpers/header.php");
+
+  try {
+    $db->makeQuery("SELECT AddCourses FROM Permissions WHERE UserId = ? LIMIT 1;", $userId);
+  }
+  catch(Exception $e) {
+    die($e.getMessage());
+  }
+
+  if($db->numRows == 1) {
+    if(!$db->result[0]["AddCourses"]) {
+      die("User not allowed to add courses.");
+    }
+  }
+
+  try {
+    $db->makeQuery("SELECT Id, Abbreviation FROM Departments");
+  }
+  catch(Exception $e) {
+    die($e.getMessage());
+  }
+
+  $departments = array();
+
+  if($db->numRows > 0) {
+    $departments = $db->result;
+  }
+  else {
+    die("No departments exist yet. You should create one.");
+  }
+
+?>
+
+  <form method="POST" action="addCourseSubmit.php">
+    <select name="department">
+
+<?php
+    foreach($departments as $department) {
+      echo '<option value="' . $department['Id'] . '">' . $department['Abbreviation'] . '</option>'
+    } 
+?>
+
+    </select>
+  </form>
+
+<?php
+
+  include("helpers/footer.php");
+?>
