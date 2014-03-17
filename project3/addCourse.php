b6561202139773a4a4cf91efb6ed99079ef46954
Stephen Morrison <srmorrisonjit@gmail.com>
2014-03-17 16:15:48 -0400

    making form nicer

--- project3/addCourse.php
+++ project3/addCourse.php
@@ -43,13 +43,13 @@
     }
 ?>
 
-    </select>
+    </select></br>
     <label for="title">Course Title</label>
-    <input type="text" id="title" name="title"></input>
+    <input type="text" id="title" name="title"></input></br>
     <label for="number">Course Number</label>
-    <input type="text" name="number" id="number" name="number">Course Number</input>
+    <input type="text" name="number" id="number" name="number"></input></br>
     <label for="description">Description</label>
-    <textarea cols="50" rows="10" id="description" name="description"></textarea>
+    <textarea cols="50" rows="10" id="description" name="description"></textarea></br>
     <label for"creditNumber">Number of Credits</label>
     <select id="creditNumber" name="creditNumber">
       <option value="" selected>Credit Number</option>
@@ -57,7 +57,7 @@
       <option value="2">2</option>
       <option value="3">3</option>
       <option value="4">4</option>
-    </select>
+    </select></br>
     <button type="submit">Create Course</button>
   </form>
 
