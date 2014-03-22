15f896d9bf3ae9cdc3e943b15cb4b8e51ede4712
Jeremy Bartz <jbartz93@gmail.com>
2014-03-22 14:14:45 -0400

    Added userlookup.php

---
+++ project3/userlookup.php
@@ -0,0 +1,50 @@
+<?php
+	$pageTitle = "Directory Search";
+	$needsToBeLoggedIn = false;
+	include('helpers/base.php');
+	include('helpers/header.php');
+?>
+<h3>Search for People</h3>
+<form method="get" action="userlookup.php">
+	<input type="text" name="q">
+	<button type="submit">Search</button>
+</form>
+<br>
+<?php
+	if(array_key_exists("q", $_GET))
+	{
+		$q = "%" . $_GET["q"] . "%";
+		$query = "select Name, Email, FullName as Department from Users join Departments on Departments.Id = DeptId where Name like ? or Email like ? or Department like ?";
+		try 
+		{
+			$db->makeQuery($query, $q, $q, $q);
+		}
+		catch(Exception $e)
+		{
+			die($e);
+		}
+		if($db->numRows > 0)
+		{
+?>
+			<table>
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Department</th>
+				</tr>
+<?php
+			foreach($db->result as $row)
+			{
+				print "<tr><td>".$row["Name"]."</td><td>".$row["Email"]."</td><td>".$row["Department"]."</td></tr>";
+			}
+?>
+			</table>
+<?php
+		}
+		else
+		{
+			print "No results found for " . $_GET["q"];
+		}
+	}
+	include('helpers/footer.php');
+?>
