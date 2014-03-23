c5614e99824ebe1b197fa0177f08aacf6c669b31
Stephen Morrison <srmorrisonjit@gmail.com>
2014-03-22 22:58:15 -0400

    fixing semesterId

--- project3/database/testData.sql
+++ project3/database/testData.sql
@@ -9,7 +9,7 @@ INSERT INTO Courses(Title, DeptId, CourseNumber, Description) VALUES('CALCULUS I
 
 INSERT INTO Semesters(Year, Season, StartDate, EndDate, Current) VALUES('2014', 'SPRING', '2014-01-20 00:00:01', '2014-05-15 00:00:01', true);
 
-INSERT INTO CourseInstances(CourseId, ProfessorId, NumberSeats, SectionNumber, Semester) VALUES(1, 1, 10, '001', 'F2014');
+INSERT INTO CourseInstances(CourseId, ProfessorId, NumberSeats, SectionNumber, SemesterId) VALUES(1, 1, 10, '001', 1);
 
 # add meeting times for the course
 
