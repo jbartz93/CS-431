f1884838f7f089eb8b6c62ff9fe2269a5cbb574a
Stephen Morrison <srmorrisonjit@gmail.com>
2014-03-24 22:25:21 -0400

    now adding meetings to the db

--- project3/addSectionSubmit.php
+++ project3/addSectionSubmit.php
@@ -8,6 +8,10 @@
   $sectionNum = $_POST['sectionNumber'];
   $professorId = intval($_POST['professor']);
   $numOfSeats = intval($_POST['numberOfSeats']);
+  $meetingLocation = $_POST['meetingLocation'];
+  $dayOfWeek = $_POST['dayOfWeek'];
+  $endTime = $_POST['endTime'];
+  $startTime = $_POST['startTime'];
 
   try {
     $db->makeQuery("SELECT Id FROM Semesters WHERE Current=true LIMIT 1;");
@@ -28,6 +32,16 @@
     $db->makeQuery("INSERT INTO CourseInstances(CourseId, ProfessorId, NumberSeats, SectionNumber, SemesterId) VALUES(?, ?, ?, ?, ?);", $courseId, $professorId, $numOfSeats, $sectionNum, $semesterId);
   } catch(Exception $e) {
     die("Error: " . $e);
+  }
+
+  $courseInstanceId = $db->insertId;
+
+  for($i = 0; $i < count($meetingLocation); $i++) {
+    try {
+      $db->makeQuery("INSERT INTO Meetings(CourseInstanceId, DayOfWeek, BeginTime, EndTime, Location) VALUES(?, ?, ?, ?, ?);", $courseInstanceId, $dayOfWeek[$i], $startTime[$i], $endTime[$i], $meetingLocation[$i]);
+    } catch(Exception $e) {
+      die("Error: " . $e);
+    }
   }
 
   header("Location: " . BASE_URL . "coursedetail.php?id=" . $courseId);
