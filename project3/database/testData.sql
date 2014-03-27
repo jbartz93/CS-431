f9b7cfc6c9c96997e761ab4206d6700006a1e130
Stephen Morrison <srmorrisonjit@gmail.com>
2014-03-27 12:40:56 -0400

    fixing test data

--- project3/database/testData.sql
+++ project3/database/testData.sql
@@ -5,13 +5,11 @@ INSERT INTO Departments(Abbreviation, FullName) VALUES('HUM', 'Humanities');
 INSERT INTO Departments(Abbreviation, FullName) VALUES('STS', 'Science and Technology');
 
 # come back and add users that have each individual role
-INSERT INTO Users(Name, Password, Email, DeptId) VALUES('John Doe', sha1('password'), 'jd@431.edu', 1);
-INSERT INTO Users(Name, Password, Email, DeptId) VALUES('George Rookie', sha1('george'), 'george@431.edu', 11);
-INSERT INTO Users(Name, Password, Email, DeptId) VALUES('Jeremy Bartz', sha1('jeremy'), 'jb@431.edu', 21);
-INSERT INTO Users(Name, Password, Email, DeptId) VALUES('Jacob Moorman', sha1('jacob'), 'jm@431.edu', 31);
-INSERT INTO Users(Name, Password, Email, DeptId) VALUES('Stephen Morrison', sha1('stephen'), 'sm@431.edu', 41);
-
-INSERT INTO Roles(UserId, Role) VALUES(1, 'student'), (11, 'faculty'), (21, 'faculty'), (31, 'staff'), (41, 'executive');
+INSERT INTO Users(Name, Password, Email, DeptId, Role) VALUES('John Doe', sha1('password'), 'jd@431.edu', 1, 'student');
+INSERT INTO Users(Name, Password, Email, DeptId, Role) VALUES('George Rookie', sha1('george'), 'george@431.edu', 11, 'faculty');
+INSERT INTO Users(Name, Password, Email, DeptId, Role) VALUES('Jeremy Bartz', sha1('jeremy'), 'jb@431.edu', 21, 'staff');
+INSERT INTO Users(Name, Password, Email, DeptId, Role) VALUES('Jacob Moorman', sha1('jacob'), 'jm@431.edu', 31, 'executive');
+INSERT INTO Users(Name, Password, Email, DeptId, Role) VALUES('Stephen Morrison', sha1('stephen'), 'sm@431.edu', 41, 'faculty');
 
 INSERT INTO Permissions(UserId, GiveGrade, ViewAllGrades, ChangeAllGrades, AddCourses) VALUES(1, true, true, true, true), (11, true, false, false, false), (21, false, true, false, false), (31, false, false, true, false), (41, false, false, false, true);
 
