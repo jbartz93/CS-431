3ef33531f5b6b8f1727ea2e6d8f7ab2daa0ab2fb
Jeremy Bartz <jbartz93@gmail.com>
2014-03-30 17:04:27 -0400

    Semicolon

--- project3/dropsubmit.php
+++ project3/dropsubmit.php
@@ -3,7 +3,7 @@
 	include('helpers/header.php');
 	$cid = $_REQUEST['courseId'];
 	$cid or die('Course Id not defined');
-	$query = "DELETE FROM Registration WHERE UserId=? AND CourseInstanceId=?;"
+	$query = "DELETE FROM Registration WHERE UserId=? AND CourseInstanceId=?;";
 	$query2 = "UPDATE CourseInstances SET NumberSeats = NumberSeats + 1 WHERE Id = ?;";
 	try
 	{
