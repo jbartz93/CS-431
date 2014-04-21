855dc063214f6cc7e3f4d56e5bcf6b6c65546936
Stephen Morrison <srmorrisonjit@gmail.com>
2014-04-21 14:22:00 -0400

    fixing locations

--- project4/tables.sql
+++ project4/tables.sql
@@ -1,6 +1,19 @@
 CREATE TABLE Airports(
   Code int(5) PRIMARY KEY,
   City varchar(60),
-  Location varchar(60),
+  LocationAbbrev varchar(60),
   Name varchar(100)
+) engine=InnoDB;
+
+CREATE TABLE CancelationCodes(
+  Code char(1) PRIMARY KEY,
+  Description varchar(25)
+) engine=InnoDB;
+
+CREATE TABLE Carriers(
+  Code varchar(4) PRIMARY KEY,
+  Name varchar(100)
+) engine=InnoDB;
+
+CREATE TABLE Locations(
 ) engine=InnoDB;
