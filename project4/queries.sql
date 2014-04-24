25f5289496c3825229e8af1f91d2d33b9ba53395
Jeremy Bartz <jbartz93@gmail.com>
2014-04-24 14:51:50 -0400

    First query works now. I had to remove the joins to the Locations table because the data in one table has quotes while the other doesn't, so none of it matches.

--- project4/queries.sql
+++ project4/queries.sql
@@ -9,19 +9,20 @@
 --the average delay duration per airline
 --cancelled/delayed flights based on cities/markets
 --we only need 2 or 3 actually written
+drop procedure if exists SearchAirports;
 delimiter //
 create procedure SearchAirports(a int(5), b int(5))
 begin
-	select Month, DayOfMonth, DayOfWeek, Carriers.Name as CarrierName, FlNum as FlightNumber, OriginAirportId, OriginAirport.Name as OriginAirportName, OriginAirport.City as OriginAirportCity, OriginMarket.City as OriginMarketCity, OriginLocation.Location as OriginAirportLocation, DestAirportId, DestAirport.Name as DestAirportName, DestAirport.City as DestAirportCity, DestMarket.City as DestAirportCity, DestLocation.Location as DestAirportLocation, TaxiOut as DepartureTime, TaxiIn as ArrivalTime
+	select Month, DayOfMonth, DayOfWeek, Carriers.Name as CarrierName, FlNum as FlightNumber, OriginAirportId, OriginAirport.Name as OriginAirportName, OriginAirport.City as OriginAirportCity, OriginMarket.City as OriginMarketCity, OriginAirport.Location as OriginAirportLocation, DestAirportId, DestAirport.Name as DestAirportName, DestAirport.City as DestAirportCity, DestMarket.City as DestMarketCity, DestAirport.Location as DestAirportLocation, TaxiOut as DepartureTime, TaxiIn as ArrivalTime
 	from Flights
 	join Airports as OriginAirport on OriginAirport.Code = OriginAirportId
 	join Airports as DestAirport on DestAirport.Code = DestAirportId
-	join Markets as OriginMarket on OriginMarket.Code = OriginMarketId
-	join Markets as DestMarket on DestMarket.Code = DestMarketId
-	join Locations as OriginLocation on OriginLocation.Abbreviation = OriginAirport.Location
-	join Locations as DestLocation on DestLocation.Abbreviation = DestAirport.Location
+	join Markets as OriginMarket on OriginMarket.Code = OriginCityMarketId
+	join Markets as DestMarket on DestMarket.Code = DestCityMarketId
+	join Carriers on Carriers.Code = UniqueCarrier
 	where OriginAirportId = a and DestAirportId = b;
 end//
+call SearchAirports(14831, 13891)//
 
 CREATE PROCEDURE FlightsOnDay(day int(1))
 BEGIN
@@ -32,6 +33,7 @@ create procedure SearchAirportsWithoutJoin(a int(5), b int(5))
 begin
 	select Month, DayOfMonth, DayOfWeek, Carriers.Name as CarrierName, FlNum as FlightNumber, @origin := OriginAirportId, @dest := DestAirportId, TaxiOut as DepartureTime, TaxiIn as ArrivalTime
 	from Flights
+	join Carriers on Carriers.Code = UniqueCarrier
 	where OriginAirportId = a and DestAirportId = b;
 	select Name, City, Locations.Location from Airports
 	join Locations on Abbreviation = Airports.Location
